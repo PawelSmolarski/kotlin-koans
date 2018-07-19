c05ad4fbf9584eb2de551a08792883b20eb69954
psmolarski <pawel.smolarski95@gmail.com>
2018-07-19 20:24:23 +0200

    16th, 17th, 18th, 19th, 20th and 21th task completed

--- src/ii_collections/n16FlatMap.kt
+++ src/ii_collections/n16FlatMap.kt
@@ -8,11 +8,11 @@ fun example() {
 }
 
 val Customer.orderedProducts: Set<Product> get() {
-    // Return all products this customer has ordered
-    todoCollectionTask()
-}
+        // Return all products this customer has ordered
+        return this.orders.flatMap { it.products }.toSet()
+    }
 
 val Shop.allOrderedProducts: Set<Product> get() {
-    // Return all products that were ordered by at least one customer
-    todoCollectionTask()
-}
+        // Return all products that were ordered by at least one customer
+        return this.customers.flatMap { it.orderedProducts }.toSet()
+    }
